01d0cff15e2fdb2984dc7199dd5a06e33005ef7b
Nayeem-Ahmed1 <nabil97773@gmail.com>
2025-02-08 19:02:11 +0600

    sitemap4

--- src/App.jsx
+++ src/App.jsx
@@ -14,11 +14,21 @@ import {
 import toast, { Toaster } from "react-hot-toast";
 import { useScreenSize } from "./utils/ScreenSizeContext";
 
+import { Helmet } from "react-helmet-async";
+
 function App() {
   const isMobile = useScreenSize();
 
   return (
     <>
+      <Helmet>
+        <title>Md. Nayeem Ahmed | Developer</title>
+        <meta
+          name="description"
+          content="The personal portfolio of Md. Nayeem Ahmed, a software developer specializing in React,Next,MongoDb,JavaScript and Vite."
+        />
+        <link rel="canonical" href="https://nayeem-portfolio-nu.vercel.app/" />
+      </Helmet>
       <BrowserRouter>
         <Toaster />
         <div className="relative z-0 bg-primary">
